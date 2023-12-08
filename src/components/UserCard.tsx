3982ef8e6dd96df663d0b19238454e7e9e24c1cd
chinagonzales <126368606+chinagonzales@users.noreply.github.com>
2023-12-08 15:50:40 +0800

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { IonAvatar, IonButton, IonIcon } from '@ionic/react';
 import { mail } from 'ionicons/icons';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { useHistory } from 'react-router-dom';
 
 interface UserCardProps {
+  userId: string;
   userName: string;
   postTime: string;
   category: string;
 }
 
-const UserCard: React.FC<UserCardProps> = ({ userName, postTime, category }) => {
+const UserCard: React.FC<UserCardProps> = ({ userId, userName, postTime, category }) => {
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState<string | null>(null);
   const history = useHistory();
 
-  const handleMessage = async () => {
-    const recipient = 'tristan';
+  useEffect(() => {
+    const fetchProfilePhoto = async () => {
+      try {
+        const userDoc = await firebase.firestore().collection('users').doc(userId).get();
+        const photoUrl = userDoc.data()?.profilePhotoUrl || null;
+        console.log('Fetched profile photo URL:', photoUrl);
+        setProfilePhotoUrl(photoUrl);
+      } catch (error) {
+        console.error('Error fetching profile photo:', error);
+      }
+    };
+  
+    fetchProfilePhoto();
+  }, [userId]);
+  
+
+  const handleMessage = () => {
+    const recipient = userId; // Use userId as the recipient
     history.push(`/messages/${recipient}`);
   };
 
   return (
     <div className="user-info">
       <IonAvatar className="avatar">
-        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+        {profilePhotoUrl ? (
+          <img alt="User's profile" src={profilePhotoUrl} />
+        ) : (
+          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+        )}
       </IonAvatar>
       <div className="user-details">
         <span className="user-name">{userName}</span>
